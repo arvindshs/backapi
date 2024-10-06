9080394e8713d205d31cbffa721f77427b2f8f75
Arul3011 <yarul8406@gmail.com>
2024-10-06 17:16:12 +0530

    updated:new

--- app/api/user/userdetails/route.js
+++ app/api/user/userdetails/route.js
@@ -3,16 +3,22 @@ import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ test: "testscucess" });
+  return NextResponse.json({ resmsg: true });
 }
 export async function POST(response) {
   const fetchdata = await response;
   const fetdata = await fetchdata.json();
   const client = await clientPromise;
   const db = await client.db("wasteuserdtabase");
-  const insertresponse = await db
-    .collection("posts")
-    .find({ userID:fetdata.userID })
+  const dbresponsre = await db
+    .collection("users")
+    .find({ _id: new ObjectId(fetdata.userID) })
     .toArray();
-  return NextResponse.json({ insertresponse });
+  if (dbresponsre)
+    return NextResponse.json({
+      dbresponsre,
+      // name: dbresponsre[0].name,
+      // email: dbresponsre[0].email,
+      // number: dbresponsre[0].mobialNum,
+    });
 }
